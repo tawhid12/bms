b500f8061f533fc482f2172d7cf27f7fcb8bb81d
tawhid12 <tawhid99@gmail.com>
2024-03-18 15:14:12 +0600

    correction

--- resources/views/front/all-products.blade.php
+++ resources/views/front/all-products.blade.php
@@ -60,24 +60,12 @@
         var filterizd = $('.filtr-container').filterizr({
             //options object
         });
-        // Listen for clicks on dynamically generated category dropdown items
-        $(document).on('click', '.dropdown-item.category', function(e) {
+        // Assuming you have a button to trigger filtering based on ID
+        // Assuming you have a dropdown to trigger filtering
+        $('.dropdown-item.category').on('click', function(e) {
                 e.preventDefault();
-                var href = $(this).attr('href');
-                alert(href)
-                var id = getParameterByName('id', href);
+                var id = $(this).data('filter');
                 filterizd.filterizr('filter', id);
             });
-
-            // Function to get URL parameter value by name
-            function getParameterByName(name, url) {
-                if (!url) url = window.location.href;
-                name = name.replace(/[\[\]]/g, "\\$&");
-                var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-                    results = regex.exec(url);
-                if (!results) return null;
-                if (!results[2]) return '';
-                return decodeURIComponent(results[2].replace(/\+/g, " "));
-            }
     </script>
 @endpush
