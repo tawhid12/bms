b387dbeba0b3ee825510eabba8d6d9f485c8e5b8
tawhid12 <tawhid99@gmail.com>
2024-03-18 13:35:46 +0600

    correction

--- resources/views/front/all-products.blade.php
+++ resources/views/front/all-products.blade.php
@@ -15,18 +15,18 @@
                 <div class="col-md-12">
                     <ul class="list-inline simplefilter">
                         <!-- For filtering controls add -->
-                        <li class="active" data-filter="all"> All items </li>
+                        <li class="active" data-filter="*"> All items </li>
                         @forelse ($categories as $c)
-                            <li data-filter="{{ $c->id }}">{{ $c->cat_name }}</li>
+                            <li data-filter=".{{ $c->id }}">{{ $c->cat_name }}</li>
                         @empty
                         @endforelse
                     </ul>
                 </div>
                 <div class="w-100">
-                    <div class="filtr-container mt-3" style="padding: 0px; position: relative;">
+                    <div class="" style="padding: 0px; position: relative;">
                         @forelse ($categories as $key => $category)
                             @forelse($category->products as $product)
-                                <div class="col-12 col-md-3 filtr-item" data-category="{{$category->id}}" data-sort="value">
+                                <div class="col-12 col-md-3 {{$category->cat_name}}" data-category="{{$category->id}}" data-sort="value">
                                     <div class="latest-slider-img">
                                         <img src="{{ asset($product->featured_image) }}" alt="" class="img-fluid w-100 h-100 img-1">
                                         <img src="{{ asset($product->featured_image_two) }}" alt="" class="img-fluid w-100 h-100 img-2">
@@ -49,9 +49,23 @@
 
 @endsection
 @push('scripts')
-    <script>
-        var filterizd = $('.filtr-container').filterizr({
-            //options object
+<script src="{{ asset('frontend/js/isotopev3.0.6.pkgd.min.js')}}"></script>	
+<script>
+    $(document).ready(function($){
+        "use strict";
+        //To Initialize the ISO Tope
+        $('.project-list').isotope();
+
+        $('.project-titles li').on('click',function(){
+            $('.project-titles li').removeClass('active');
+            $(this).addClass('active');
+            
+            var selector = $(this).attr('data-filter');
+            $('.project-list').isotope({
+                filter:selector
+            });
         });
-    </script>
+    }(jQuery));
+</script>
+    
 @endpush
