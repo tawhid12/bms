3ac3fe02f8068116db1b5a1e03a115da02c9330e
tawhid12 <tawhid99@gmail.com>
2024-03-18 14:39:05 +0600

    correction

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -81,9 +81,21 @@
         @endif
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light">
+                {{-- <a class="navbar-brand" href="{{route('front')}}">
+                    <img src="{{ asset('assets/images/logo.png') }}" alt="logo" class="logo">
+                </a> --}}
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto bg-light bg-transparent-md">
 
-
+ 
+                    </ul>
+                </div>
             </nav>
         </div>
         </div>
