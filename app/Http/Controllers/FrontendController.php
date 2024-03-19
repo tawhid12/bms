5ede707655b76c630cd56414fd68aa2722e894b6
tawhid12 <tawhid99@gmail.com>
2024-03-19 10:28:46 +0600

    correction

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -94,7 +94,7 @@ class FrontendController extends Controller
     public function allproducts(){
         $categories = Category::with(['products' => function ($query) {
             $query->orderBy('serial', 'asc'); // Replace 'your_column_name' with the column you want to order by
-        },'ebrochure'])->get();
+        }])->get();
         return view('front.all-products',compact('categories'));
     }
     public function productbycat($id){
