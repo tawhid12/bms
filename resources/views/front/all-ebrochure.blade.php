778df4251ef755cd4ad02820be7c679e4f607d70
tawhid12 <tawhid99@gmail.com>
2024-03-19 10:43:33 +0600

    correction

--- resources/views/front/all-ebrochure.blade.php
+++ resources/views/front/all-ebrochure.blade.php
@@ -31,12 +31,12 @@
                     <h2>Ebrochure</h2>
                 </div>
                 @forelse($ebrochures as $e)
-                    <a href="{{ asset($e->upload_pdf) ?? '' }}">
-                        <div class="col-12 col-md-3">
-                            <img src="{{ asset($e->image) }}" class="img-fluid">
-                            <h3>{{ $e->title }}</h3>
-                        </div>
-                    </a>
+                    <div class="col-12 col-md-3">
+                        <a href="{{ asset($e->upload_pdf) ?? '' }}">
+                        <img src="{{ asset($e->image) }}" class="img-fluid">
+                        <h3>{{ $e->title }}></h3>
+                                </a>
+                    </div>
                 @empty
                     <div class="col-md-12 my-3">
                         <h4>No Brochure Found</h4>
